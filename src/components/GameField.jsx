28d765bdc689fab7e26054baa02fc02ae16ebdbd
Anatoli Avetisovi <tolikatolika794@gmail.com>
2022-07-21 14:39:50 +0400

    added questions to game

--- src/components/GameField.jsx
+++ src/components/GameField.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from "react";
+import { GameContext } from "../context";
+import questions from "../questions";
 
 const GameField = () => {
-  return (
-    <div>GameField</div>
-  )
-}
+  const [question, setQuestion] = useState({ question: "", answers: {} });
+  const { questionCount, setQuestionCount, setGameStatus } = useContext(GameContext);
 
-export default GameField
+
+  useEffect(() => {
+    setQuestion(questions[questionCount - 1]);
+  }, [questionCount]);
+
+  function submitAnswer(e) {
+    e.preventDefault();
+    if (questionCount === questions.length) {
+      setGameStatus('finish')
+    } else {
+      setQuestionCount(prev => prev + 1)
+    }
+  }
+  return (
+    <div className="game__field">
+      <h2 className="game__title">{question.question}</h2>
+      <form className="game__answers" onSubmit={submitAnswer}>
+        {Object.entries(question.answers).map((answer) => (
+          <div key={answer[0]} className="game__answer">
+            <input
+              type="radio"
+              id={answer[0]}
+              name="answer"
+              value={answer[0]}
+            />
+            <label htmlFor={answer[0]}>{answer[1]}</label>
+          </div>
+        ))}
+        <button>Submit answer!</button>
+      </form>
+    </div>
+  );
+};
+
+export default GameField;
